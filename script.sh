82e8da4a5644ccdedeee7fec9dd4e87d4b32f7d0
Wayne Connolly <wayne@wayneconnolly.com>
2018-06-29 23:45:15 +1000

    removed rancher-compose baking

--- script.sh
+++ script.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-#curl -o rancher-compose.tar.gz https://github.com/rancher/rancher-compose/releases/download/v0.12.5/rancher-compose-linux-amd64-v0.12.5.tar.gz
-
-tar -zxvf rancher-compose.tar.gz
-mv rancher-compose-*/rancher-compose /bin/.
-chmod +x /bin/rancher-compose
 git clone ${PLUGIN_REPO} /rancher
 /bin/rancher-compose \
  --url http://${PLUGIN_URL}/v1 \
