73ea8e1fca80a3d199266f0fd2b2cf1f9e7f577d
Wayne Connolly <wayne@wayneconnolly.com>
2018-06-29 22:43:00 +1000

    updated

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@
 tar -zxvf rancher-compose.tar.gz
 mv rancher-compose-*/rancher-compose /bin/.
 chmod +x /bin/rancher-compose
-git clone ${PLUGIN_GIT}
+git clone ${PLUGIN_REPO} /repo
 /bin/rancher-compose \
  --url http://${PLUGIN_URL}/v1 \
- --access-key ${PLUGIN_ACCESS} \
- --secret-key ${PLUGIN_SECRET} \
- -f /stack/docker-compose.yml -r /stack/rancher-compose.yml \
+ --access-key ${PLUGIN_ACCESSKEY} \
+ --secret-key ${PLUGIN_SECRETKEY} \
+ -f /repo/${PLUGIN_PATH}/docker-compose.yml -r /repo/${PLUGIN_PATH}/rancher-compose.yml \
  --verbose -p ${PLUGIN_STACK} up -p -d --force-upgrade --confirm-upgrade
