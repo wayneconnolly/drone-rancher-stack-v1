de3daa1aafab854f7163b729b8c826960ee90f45
Wayne Connolly <wayne@wayneconnolly.com>
2018-07-01 00:09:47 +1000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-find / -name docker-compose.yml
-echo "pwd" && pwd
-echo "ls" && ls /
-echo "ls rancher" && ls /rancher/
-cd /rancher && ls .
+DOCKER_COMPOSE=`find / -name docker-compose.yml`
+RANCHER_COMPOSE=`find / -name rancher-compose.yml`
+cat ${DOCKER_COMPOSE}
+cat ${RANCHER_COMPOSE}
+echo "ls rancher" && ls rancher/
 /bin/rancher-compose \
  --url http://${PLUGIN_URL}/v1 \
  --access-key ${PLUGIN_ACCESSKEY} \
  --secret-key ${PLUGIN_SECRETKEY} \
- -f /rancher/${PLUGIN_PATH}/docker-compose.yml -r /rancher/${PLUGIN_PATH}/rancher-compose.yml \
+ -f ${DOCKER_COMPOSE} -r ${RANCHER_COMPOSE} \
  --verbose -p ${PLUGIN_STACK} up -p -d --force-upgrade --confirm-upgrade
