6c4f5e091f10d6eecf7d08a04fa8b300de7bcc20
Wayne Connolly <wayne@wayneconnolly.com>
2019-02-24 17:32:57 +1100

    Added feature to delete stack if in degraded state

--- script.sh
+++ script.sh
@@ -19,5 +19,14 @@ echo ""
 echo "docker-compose.yml @ ${DOCKER_COMPOSE}"
 cat ${DOCKER_COMPOSE}
 echo ""
-echo "Deploying Rancher Stack with force upgrade"
-/bin/rancher --url http://${PLUGIN_URL} --access-key ${ACCESSKEY} --secret-key ${SECRETKEY} up --stack ${PLUGIN_STACK} -d -f ${DOCKER_COMPOSE} --rancher-file ${RANCHER_COMPOSE} --pull --force-recreate --confirm-upgrade
+
+/bin/rancher --url http://${PLUGIN_URL} --access-key ${ACCESSKEY} --secret-key ${SECRETKEY} stacks ls > status
+
+if grep -q degraded status; then
+    echo 'Stack is degraded. Killing stack now!'
+    ID=`sed -e 's/\s.*$//' status`
+    /bin/rancher --url http://${PLUGIN_URL} --access-key ${ACCESSKEY} --secret-key ${SECRETKEY} rm $ID
+else
+    echo 'Stack healthy or not found. Creating/Updating stack with force upgrade'
+    /bin/rancher --url http://${PLUGIN_URL} --access-key ${ACCESSKEY} --secret-key ${SECRETKEY} up --stack ${PLUGIN_STACK} -d -f ${DOCKER_COMPOSE} --rancher-file ${RANCHER_COMPOSE} --pull --force-recreate --confirm-upgrade
+fi
